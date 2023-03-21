394e7766d8c23e48220cc80b27158e901ef205e4
dellancelod <danil.svetaylo@gmail.com>
2023-03-21 11:58:00 +0200

    Create tuning component

--- tune-hub/src/app/app.module.ts
+++ tune-hub/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TuningComponent } from './tuning/tuning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TuningComponent
   ],
   imports: [
     BrowserModule,
