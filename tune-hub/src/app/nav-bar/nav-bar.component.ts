22eb95c025d720c152c8a8044c96cdaf288a29e9
dellancelod <danil.svetaylo@gmail.com>
2023-03-22 09:43:53 +0200

    Inject nav bar service

--- tune-hub/src/app/nav-bar/nav-bar.component.ts
+++ tune-hub/src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { NavBarService } from '../services/nav-bar.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
-
+export class NavBarComponent implements OnInit{
+  constructor(public navBarService: NavBarService) { }
+ 
+  ngOnInit() {
+  }
 }
